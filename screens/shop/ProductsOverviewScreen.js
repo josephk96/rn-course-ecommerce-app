f5a4cdb8379ea7d5b0e398869b8e6aac3a9a5b58
Soo Hwan Kim <kimsoohwan10@gmail.com>
2020-08-14 01:36:13 +0900

    Add Redux store, action and reducer files and mock data

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = (props) => {
+  return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
